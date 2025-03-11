2ec1aa5af22634bcdc074b2dcc99349c12a842c7
emi-pereyra17 <emiacebal2012@hotmail.com>
2025-03-11 06:31:50 -0300

    Configuraciones

--- src/main/java/com/bazar/proyectoBazar/ProyectoBazarApplication.java
+++ src/main/java/com/bazar/proyectoBazar/ProyectoBazarApplication.java
@@ -2,12 +2,30 @@ package com.bazar.proyectoBazar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ProyectoBazarApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoBazarApplication.class, args);
+	}
+
+	@Configuration
+	public static class Myconfiguration{
+		@Bean
+		public WebMvcConfigurer corsConfigurer(){
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
 	}
 
 }
